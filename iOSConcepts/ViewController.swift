c2e9ac9ef59edcebbeffc4112e1c95996e635337
Avijit Nagre <avijit.nagre@weatherford.com>
2019-02-12 16:15:34 +0530

    Initial commits

---
+++ iOSConcepts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSConcepts
+//
+//  Created by Avijit Nagare on 12/02/19.
+//  Copyright © 2019 Avijit Nagare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
